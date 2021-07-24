db8590a08cbc4fb5dec916458e58eadbc3c0f702
missioncoding <er.zeelumak@gmail.com>
2021-07-24 14:43:36 +0530

    Updates towards Home screen and adding all routes to controller js file

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -13,6 +13,21 @@ class Controller extends Component {
             <Router>
                 <div className= "main-container">
                     <Route exact path="/" render={props=> <Home {...props} baseUrl={this.baseUrl}/>} />
+                    <Route exact path='/checkout' render={({history},props) => (
+                        sessionStorage.getItem('access-token') === null ? (
+                            <Redirect to='/' />
+                        ) : (
+                                <Checkout {...props} baseUrl={this.baseUrl} history={history} />
+                            )
+                    )} />
+                    <Route exact path='/restaurant/:id' render={({history},props) => <Details {...props} baseUrl={this.baseUrl} history={history} />} />
+                    <Route path='/profile' render={({history},props) => (
+                        sessionStorage.getItem('access-token') === null ? (
+                            <Redirect to='/' />
+                        ) : (
+                                <Profile {...props} baseUrl={this.baseUrl} history={history} />
+                            )
+                    )} />
                 </div>
             </Router>
         );
